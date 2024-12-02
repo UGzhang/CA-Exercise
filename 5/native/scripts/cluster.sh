4d1837a20b4abcef64959f086a91731e02eda701
UGzhang <aaron.zhang.edu@outlook.com>
2024-12-02 23:14:25 +0100

    upload hw5 native jacobi

---
+++ 5/native/scripts/cluster.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+  
+#SBATCH --partition=singlenode
+#SBATCH --time=00:02:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --exclusive
+#SBATCH --cpu-freq=2400000-2400000:performance
+#SBATCH --export=NONE
+
+# Enable debug and verbose mode
+set -x
+set -v
+
+# To ensure 'srun' runs as expected
+export SLURM_EXPORT_ENV=ALL
+export SRUN_CPUS_PER_TASK=1  # value should match the SBATCH --cpus-per-task option
+
+
+module load intel
+make clean -C ../
+make -C ../
+
+
+../bin/jacobi 1024
+gprof ../bin/jacobi ./gmon.out > gprof_analysis.txt
+
