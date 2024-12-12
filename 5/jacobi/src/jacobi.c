936f91f34688bacc5281d6e41478b259923cea16
UGzhang <aaron.zhang.edu@outlook.com>
2024-12-12 21:00:35 +0100

    add novector at inner loop

--- 5/jacobi/src/jacobi.c
+++ 5/jacobi/src/jacobi.c
@@ -12,6 +12,8 @@ void jacobi(const double * restrict grid_source, double * restrict grid_target, 
 #pragma nounroll
 #pragma novector
 	for(int32_t j = 1; j < y-1; j++){
+		#pragma nounroll
+		#pragma novector
 		for(int32_t i = 1; i < x-1; i++){
 			grid_target(i,j) = 0.25 * (grid_source(i-1,j)+grid_source(i+1,j)+grid_source(i,j-1)+grid_source(i,j+1));
 		}
@@ -20,6 +22,8 @@ void jacobi(const double * restrict grid_source, double * restrict grid_target, 
 #pragma nounroll
 #pragma novector
 	for(int32_t i = 1; i < x-1; i++){
+		#pragma nounroll
+		#pragma novector
 		for(int32_t j = 1; j < y-1; j++){
 			grid_target(i,j) = 0.25 * (grid_source(i-1,j)+grid_source(i+1,j)+grid_source(i,j-1)+grid_source(i,j+1));
 		}
